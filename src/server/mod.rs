c7806926e1eef52740e66d50eddb14fe45d0b7ac
Gokay Okyay <gokayokyay@hotmail.com>
2021-04-10 15:01:21 +0000

    (Server) changed reports for task endpoint

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -38,7 +38,7 @@ impl Server {
             .post("/execute", exec_task)
             .post("/abort", abort_task)
             .get("/activetasks", get_active_tasks)
-            .get("/reports/:id", get_reports_for_task)
+            .get("/task/:id/reports", get_reports_for_task)
             .get("/reports", get_reports)
             // .err_handler_with_info(error_handler)
             .build()
